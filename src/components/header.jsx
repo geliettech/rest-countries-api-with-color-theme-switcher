9663b3f78b2839da6067dce098acbe04486e703d
julietoge <julietogechi27@gmail.com>
2024-11-08 16:55:04 +0100

    install @fortawesome/react-fontawesome

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import styles from "../App.module.css";
 import { Link } from "react-router-dom";
-import { IoMoonOutline } from "react-icons/io5";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <h1 className="title">Where in the world?</h1>
-      <div className="darkmode">
+      <h1 className={styles.title}>Where in the world?</h1>
+      <div className={styles.darkmode}>
         <span>
-          {/* <i class="fa fa-moon-o" aria-hidden="true"></i> */}
-          Your <FontAwesomeIcon icon="coffee" /> is hot and ready!
+          <FontAwesomeIcon icon={faMoon} size="lg" />
         </span>
-        <span>darkmode</span>
+        <span>Dark Mode</span>
       </div>
     </div>
   );
