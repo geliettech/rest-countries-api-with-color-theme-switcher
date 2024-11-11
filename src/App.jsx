69ac9ed1b31ef4423a62fa488effed228ebd4fea
julietoge <julietogechi27@gmail.com>
2024-11-11 18:39:27 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,10 @@ import CountryDetails from "./pages/countryDetails";
 import Layout from "./components/layout";
 
 const App = () => {
-  // const [isDarkMode, setIsDarkMode] = useState(false);
-
-  // // Apply dark mode styles to the body element
-  // useEffect(() => {
-  //   if (isDarkMode) {
-  //     document.body.classList.add('dark-mode');
-  //     document.body.classList.remove('light-mode');
-  //   } else {
-  //     document.body.classList.add('light-mode');
-  //     document.body.classList.remove('dark-mode');
-  //   }
-  // }, [isDarkMode]);
-
-  // const toggleTheme = () => setIsDarkMode(!isDarkMode);
 
   return (
     <Layout className={styles.App}>
       <BrowserRouter>
-      {/* <ThemeToggle toggleTheme={toggleTheme} isDarkMode={isDarkMode} /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/country/:numericCode" element={<CountryDetails />} />
@@ -36,11 +21,3 @@ const App = () => {
 
 export default App;
 // https://mui.com/material-ui/customization/dark-mode/
-// Your users should be able to:
-
-// - See all countries from the API on the homepage
-// - Search for a country using an `input` field
-// - Filter countries by region
-// - Click on a country to see more detailed information on a separate page
-// - Click through to the border countries on the detail page
-// - Toggle the color scheme between light and dark mode *(optional)*
