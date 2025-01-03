7892b2ff763593ef8290f1f0adc137f9a9356a12
julietoge <julietogechi27@gmail.com>
2025-01-03 14:48:49 +0100

    uninstall fontawesome

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,8 +3,10 @@ import Footer from "./footer";
 import { useDarkMode } from "./DarkModeContext";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Button, CssBaseline, Paper, Typography } from "@mui/material";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon } from "@fortawesome/free-regular-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faMoonFill } from "@fortawesome/free-regular-svg-icons";
+import { IconName } from "react-icons/bs";
+
 
 const Layout = ({ children }) => {
   const { darkMode, toggleDarkMode } = useDarkMode();
@@ -22,16 +24,34 @@ const Layout = ({ children }) => {
         style={{
           display: "flex",
           justifyContent: "space-between",
-          alignItems: "center", padding: 18
+          alignItems: "center",
+          padding: 18,
         }}
       >
-        <Typography  style={{
-         fontSize: 28,
-        }} variant="h1">
+        <Typography
+          style={{
+            fontSize: 28,
+          }}
+          variant="h1"
+        >
           Where in the world?
         </Typography>
-        <Button onClick={toggleDarkMode} variant="contained" color="primary">
-          <FontAwesomeIcon icon={faMoon} size="lg" />
+        <Button
+          onClick={toggleDarkMode}
+          variant="contained"
+          sx={{
+            border: "none",
+            boxShadow: "none",
+            "&:hover": {
+              border: "none",
+              boxShadow: "none",
+            },
+            backgroundColor: darkMode ? "#000" : "#fff",
+            color: darkMode ? "#fff" : "#000",
+          }}
+        >
+          <FontAwesomeIcon icon={faMoonFill} size="lg"  style={{ color: "blue" }} />
+          {/* {darkMode ? "Light Mode" : "Dark Mode"} */}
           Dark Mode
         </Button>
       </Paper>
