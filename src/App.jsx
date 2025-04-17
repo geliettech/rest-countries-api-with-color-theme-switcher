0c87c4bf83b224fea4135fb47d0463194655160a
julietoge <julietogechi27@gmail.com>
2025-04-17 08:33:20 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+// Importing necessary dependencies from React and other packages
 import React from "react";
-import "./styles/globals.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import CountryDetails from "./pages/countryDetails";
-import { DarkModeProvider } from "./components/DarkModeContext";
+import "./styles/globals.css"; // Importing global styles for the application
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Importing routing components from react-router-dom
+import Home from "./pages/home"; // Importing Home component
+import CountryDetails from "./pages/countryDetails"; // Importing CountryDetails component
+import { DarkModeProvider } from "./components/DarkModeContext"; // Importing the DarkModeProvider to manage dark mode state
 
+// Main App component
 const App = () => {
   return (
+    // Wrapping the entire application in the DarkModeProvider to provide dark mode context to all components
     <DarkModeProvider className="App">
+      {/* BrowserRouter component is used to enable routing in the application */}
       <BrowserRouter>
+        {/* Routes component is used to define all the routes in the app */}
         <Routes>
+          {/* Defining the root route ("/") to render the Home component */}
           <Route path="/" element={<Home />} />
+          
+          {/* Defining a route for the CountryDetails page. The route includes a dynamic parameter 'numericCode' */}
           <Route path="/country/:numericCode" element={<CountryDetails />} />
         </Routes>
       </BrowserRouter>
@@ -18,4 +26,5 @@ const App = () => {
   );
 };
 
+// Exporting the App component to use in other parts of the application
 export default App;
